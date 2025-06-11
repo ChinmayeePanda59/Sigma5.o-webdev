8a5fcc221d0b3299f5b1e5248802303fbe54c10c
ChinmayeePanda59 <chinmayeeofficial59@gmail.com>
2025-06-11 22:02:15 +0530

    JS 1.3

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,43 @@
-todo = [];
+// todo = [];
 
-let req;
- req=prompt("Enter the action you wanna perform");
+// let req;
+//  req=prompt("Enter the action you wanna perform");
    
 
 
-while(true){
+// while(true){
 
-    if(req== "quit"){
-        console.log("Quitting app");
-        break;
-    }
+//     if(req== "quit"){
+//         console.log("Quitting app");
+//         break;
+//     }
 
-if(req== "list"){
-    console.log("----------------------");
-    for(task of todo){  
-       console.log(task);}
-    console.log("----------------------");}
+// if(req== "list"){
+//     console.log("----------------------");
+//     for(task of todo){  
+//        console.log(task);}
+//     console.log("----------------------");}
 
-else if(req == "add"){
-    task=prompt("Enter the task you wanna add");
-    todo.push(task);}
+// else if(req == "add"){
+//     task=prompt("Enter the task you wanna add");
+//     todo.push(task);}
 
  
     
-req=prompt("Enter the action you wanna perform");
-}
+// req=prompt("Enter the action you wanna perform");
+// }
+
+const max =prompt("Enter the maximum number till which you wanna guess");
+const random = Math.floor(Math.random()*max)+1;
+
+
+let guess = prompt("Guess the number");
+
+while(true){
+    if(guess == random){
+        console.log("You are right");
+
+    }
+
+
+    
